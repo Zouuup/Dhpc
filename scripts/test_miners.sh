c2e1601b86ea34e6a01d007b910f60b69f0cb800
Armin Ranjbar <zoup@zoup.org>
2023-06-01 02:15:10 +0100

    adding test for miners

---
+++ scripts/test_miners.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Test
+
+Dhpcd tx data add-data 0xfe13119fb084fe8bbf5fe3ab7cc89b3b dhpc1sr55zk85uerru8tu0m7l7aujha5f8chcxh0073 ethereum any 150 --from dataProvider1 -y --log_level error  --gas 500237 
+
+Dhpcd tx data add-data 0x3b5d5c3712955042212316173ccf37be dhpc1g6veqgqucfsx987uz9r03q8hesy8798c5e5c7l ethereum any 150 --from dataProvider2 -y --log_level error  --gas 500237
+
+Dhpcd tx data add-data 0x2cd6ee2c70b0bde53fbe6cac3c8b8bb1 dhpc16stssnndzktenrpjpy0sk84pvqh8800u0h80s3 ethereum any 150 --from dataProvider3 -y --log_level error  --gas 500237
+
+Dhpcd tx request create-treasury `Dhpcd keys show treasury -a` -y --log_level error  --from alice
+
+Dhpcd tx request create-allowed-oracles main `Dhpcd keys show oracle -a` --from alice -y --log_level error 
+
+Dhpcd tx request create-request-record aea7cfd3-1e63-4f90-9311-1c32dacf41cb 0xa0fb4b5e51e55405e5b9e6c5defe381409e99204a3a23b5ef994e3951ded26e0 ethereum `Dhpcd keys show dapp -a` 0xc436eb8aed128275c8f224de2f1dd202c0ab5830 `Dhpcd keys show oracle -a` 12345678 --from oracle -y --log_level error  --gas 500237
+
